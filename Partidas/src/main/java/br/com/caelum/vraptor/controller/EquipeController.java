10be6eaeeb5b7c032d60080c4dcf6cdca23d4277
Victor Baesso <jotatyr@gmail.com>
2019-03-21 22:04:40 -0300

    Foi o Vitor paulo, não o jao baesso

--- Partidas/src/main/java/br/com/caelum/vraptor/controller/EquipeController.java
+++ Partidas/src/main/java/br/com/caelum/vraptor/controller/EquipeController.java
@@ -1,10 +1,37 @@
 package br.com.caelum.vraptor.controller;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
+import br.com.vitor.vitorjoao.banco.Banco;
+import br.com.vitor.vitorjoao.model.Equipe;
 
 @Path("/equipe")
 @Controller
 public class EquipeController {
 
+	@Inject
+	Result result;
+
+	@Get("/listar")
+	public List<Equipe> listar() {
+		return Banco.listaEquipes;
+	}
+
+	@Get("/editar/{id}")
+	public void editar(Long id) {
+		result.include("equipe", Banco.getEquipeById(id));
+	}
+
+	@Post("/salvar")
+	public void salvar(Equipe equipe) {
+		Banco.setEquipe(equipe);
+		result.forwardTo(IndexController.class).index();
+	}
 }
--- Partidas/src/main/java/br/com/vitor/vitorjoao/banco/Banco.java
+++ Partidas/src/main/java/br/com/vitor/vitorjoao/banco/Banco.java
@@ -20,6 +20,23 @@ public class Banco {
 		return null;
 	}
 
+	public static Equipe getEquipeById(Long id) {
+		for (Equipe e : listaEquipes) {
+			if (e.getId() == id) {
+				return e;
+			}
+		}
+		return null;
+	}
+
+	public static void setEquipe(Equipe eq) {
+		for (Equipe e : listaEquipes) {
+			if (e.getId() == eq.getId()) {
+				e.setNome(eq.getNome());
+			}
+		}
+	}
+
 	public void init() {
 		Equipe eq1 = new Equipe();
 		eq1.setNome("Vitor");
